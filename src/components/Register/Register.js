dd664bc5a5c741d891547fb68fc340ec51d2a46c
Leo Taylor <leotaylor723@gmail.com>
2018-07-12 18:55:57 -0500

    added 4 components

---
+++ src/components/Register/Register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Register.css';
+
+class Register extends React.Component {
+  render () {
+    return (
+      <h1 className='Register'>Register</h1>
+    );
+  }
+}
+
+export default Register;
