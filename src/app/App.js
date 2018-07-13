22a768f1f7b464d8862b339d07451c676a5c8176
Leo Taylor <leotaylor723@gmail.com>
2018-07-12 19:33:17 -0500

    private route, redirect to login if not authed

--- src/app/App.js
+++ src/app/App.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
-import {Route, BrowserRouter, Switch} from 'react-router-dom';
+import {Route, BrowserRouter, Redirect, Switch} from 'react-router-dom';
 import './App.css';
 
 import Navbar from '../components/Navbar/Navbar';
 // import AllTheStuff from '../components/AllTheStuff/AllTheStuff';
-// import MyStuff from '../components/MyStuff/MyStuff';
+import MyStuff from '../components/MyStuff/MyStuff';
 // import Login from '../components/Login/Login';
 // import Register from '../components/Register/Register';
 import Home from '../components/Home/Home';
 
+const PrivateRoute = ({ component: Component, authed, ...rest}) => {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        authed === true ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{ pathname: '/login', state: {from: props.location}}}
+          />
+        )
+      }
+    />
+  );
+};
+
 class App extends Component {
+  state={
+    authed: false,
+  }
+
   render () {
     return (
       <div className="App">
         {/* <AllTheStuff /> */}
-        {/* <MyStuff /> */}
+        <MyStuff />
         {/* <Login /> */}
         {/* <Register /> */}
         <BrowserRouter>
@@ -24,6 +45,11 @@ class App extends Component {
               <div className="row">
                 <Switch>
                   <Route path="/" exact component={Home} />
+                  <PrivateRoute
+                    path='/mystuff'
+                    authed={this.state.authed}
+                    componenet={MyStuff}
+                  />
                 </Switch>
               </div>
             </div>
