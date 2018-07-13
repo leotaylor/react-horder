14bfb71fe5ae7e60873fb7d287a08c6b61d72f4b
Leo Taylor <leotaylor723@gmail.com>
2018-07-13 10:27:24 -0500

    navigation works, I cannot spell component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,7 +6,7 @@ class Home extends React.Component {
   render () {
     return (
       <div className='Home'>
-        <h1 >My Precious</h1>
+        <h1>My Precious</h1>
       </div>
     );
   }
